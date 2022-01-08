e49a84a5890bc4c7c9a201a336beebcd27e3b7b6
ranemr <ranemriad@gmail.com>
2022-01-08 16:29:09 +0100

    2 urls added which refer to new functions

--- order/urls.py
+++ order/urls.py
@@ -9,5 +9,7 @@ from . import views
 urlpatterns = [
     path('', views.order_list ),
     path('add_order/', views.add_order, name='add_order'),
+    path('update_order/<str:pk>', views.update_order, name='modify_order'),
+    path('delete_order/<str:pk>', views.delete_order, name='delete_order'),
 
 ]

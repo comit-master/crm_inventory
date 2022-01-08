8fff54b9797d987a41ad48a494beb7efecd70c61
ranemr <ranemriad@gmail.com>
2022-01-08 15:22:36 +0100

    add_order function update, loop which save input from user, added on dico and render function update

--- order/views.py
+++ order/views.py
@@ -1,9 +1,21 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+from order.forms import OrderForm
+
+
 def order_list(request):
     return render(request, 'order/order_list.html')
 
 def add_order(request):
-    return render(request, 'order/order_add.html')
+    form= OrderForm()
+    if request.method=='POST':
+        form= OrderForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    context={'order_form':form}
+    return render(request, 'order/order_add.html', context)
+

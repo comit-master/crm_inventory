93b5520f8f19a6764e763d814df6d45a255fc6f8
ranemr <ranemriad@gmail.com>
2022-01-08 14:45:55 +0100

    new html to add an order, views update to refer to this new front, new url added with tag on html file

--- order/urls.py
+++ order/urls.py
@@ -8,6 +8,6 @@ from . import views
 
 urlpatterns = [
     path('', views.order_list ),
-
+    path('add_order/', views.add_order, name='add_order'),
 
 ]
--- order/views.py
+++ order/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 # Create your views here.
 def order_list(request):
     return render(request, 'order/order_list.html')
+
+def add_order(request):
+    return render(request, 'order/order_add.html')

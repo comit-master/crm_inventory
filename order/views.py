724a1c1b1f3e2916d2ec47685385bdc9009eff52
ranemr <ranemriad@gmail.com>
2022-01-08 16:34:31 +0100

    2 new functions added which allows manager to modify order or delete it

--- order/views.py
+++ order/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect
 
 # Create your views here.
 from order.forms import OrderForm
+from .models import Order
 
 
 def order_list(request):
@@ -18,4 +19,25 @@ def add_order(request):
 
     context={'order_form':form}
     return render(request, 'order/order_add.html', context)
+
+
+def update_order(request, pk):
+    order= Order.objects.get(id=pk)
+    form= OrderForm(instance=order)
+    if request.method=='POST':
+        form= OrderForm(request.POST, instance=order)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    context={'order_form':form}
+    return render(request, 'order/order_add.html', context)
+
+def delete_order(request, pk):
+    order= Order.objects.get(id=pk)
+    if request.method=='POST':
+        order.delete()
+        return redirect('/')
+    context={'item':order}
+    return render(request, 'order/delete_order.html', context)
 

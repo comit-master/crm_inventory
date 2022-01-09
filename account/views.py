d93092bd5cd2ada4e03b576236b5ac3be6aff248
ranemr <ranemriad@gmail.com>
2022-01-09 10:50:57 +0100

    encryption data for security and update loginpage function to authenticate user with db info and redirect to homepage

--- account/views.py
+++ account/views.py
@@ -1,6 +1,8 @@
+from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from .forms import CreateUser
+from django.contrib import messages
 
 # Create your views here.
 def RegistrationPage(request):
@@ -9,11 +11,23 @@ def RegistrationPage(request):
         form= CreateUser(request.POST)
         if form.is_valid():
             form.save()
+            user=form.cleaned_data.get('username')
+            messages.success(request, 'Account created with success ' + user)
             return redirect('login')
     context={'form':form}
     return render(request, 'account/registration.html', context)
 
 def LoginPage(request):
-    context={}
-    return render(request, 'account/login.html', context)
+    form= CreateUser()
+    if request.method=='POST':
+        username= request.POST.get('username')
+        password=request.POST.get('password')
+        user= authenticate(request, username=username, password=password) #verifie si cohérence entre l'input de
+        # l'user et ce qu'il ya dans la base de données
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request, 'User and/or password not valid')
+    return render(request, 'account/login.html')
 

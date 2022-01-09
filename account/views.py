aaf5d0a8969391a474b0be72754f9aadb1cdb1bf
ranemr <ranemriad@gmail.com>
2022-01-09 10:12:58 +0100

    update registration, encryption data, display error if password not valid + update view to register user + redirect to login page

--- account/views.py
+++ account/views.py
@@ -1,10 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from .forms import CreateUser
 
 # Create your views here.
 def RegistrationPage(request):
-    form= CreateUser(request.POST)
+    form= CreateUser()
+    if request.method=='POST':
+        form= CreateUser(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
     context={'form':form}
     return render(request, 'account/registration.html', context)
 
